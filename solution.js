b47a49922b20a642607c27318b687b5b7b551324
ju8ko <julia.x.kobzar@gmail.com>
2022-08-22 18:45:26 +0200

    The exercise is done

---
+++ solution.js
@@ -0,0 +1,9 @@
+//Write a JavaScript program to calculate the factorial of a number.
+function factorial(num) {
+  if (num === 0) {
+    return 1;
+  } else {
+    return num * factorial(num - 1);
+  }
+}
+console.log(factorial(5));
